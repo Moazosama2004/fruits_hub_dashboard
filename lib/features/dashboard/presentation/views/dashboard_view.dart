c8321b6b80011c87dca35ab9932d7fb3dd2a8c4c
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2025-04-21 05:45:07 +0200

    first commit

---
+++ lib/features/dashboard/presentation/views/dashboard_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub_dashboard/features/dashboard/presentation/views/widget/dashboard_view_body.dart';
+
+class DashboardView extends StatelessWidget {
+  const DashboardView({super.key});
+  static const routeName = 'dashboardView';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: DashboardViewBody());
+  }
+}
