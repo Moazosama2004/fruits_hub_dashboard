3569d0c06b64de51509a3485970ebf3a0c18510a
Muaz osama <moaz21-01130@student.eelu.edu.eg>
2025-05-02 05:33:54 +0300

    Create update order cubit

---
+++ lib/features/orders/presentation/manager/update_order_cubit/update_order_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:fruits_hub_dashboard/core/enums/order_enum.dart';
+import 'package:fruits_hub_dashboard/features/orders/domain/repos/orders_repo.dart';
+import 'package:meta/meta.dart';
+
+part 'update_order_state.dart';
+
+class UpdateOrderCubit extends Cubit<UpdateOrderState> {
+  UpdateOrderCubit(this.ordersRepo) : super(UpdateOrderInitial());
+  final OrdersRepo ordersRepo;
+
+  updateData({required OrderStatusEnum status, required String orderId}) async {
+    emit(UpdateOrderLoading());
+    var result = await ordersRepo.updateOrder(status: status, orderId: orderId);
+    result.fold(
+      (f) => emit(UpdateOrderFailure(f.errMessage)),
+      (v) => emit(UpdateOrderSuccess()),
+    );
+  }
+}
--- lib/features/orders/presentation/views/orders_view.dart
+++ lib/features/orders/presentation/views/orders_view.dart
@@ -3,8 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub_dashboard/core/services/get_it_service.dart';
 import 'package:fruits_hub_dashboard/features/orders/domain/repos/orders_repo.dart';
 import 'package:fruits_hub_dashboard/features/orders/presentation/manager/fetch_orders_cubit/fetch_orders_cubit.dart';
+import 'package:fruits_hub_dashboard/features/orders/presentation/manager/update_order_cubit/update_order_cubit.dart';
 import 'package:fruits_hub_dashboard/features/orders/presentation/views/widgets/orders_view_body.dart';
 import 'package:fruits_hub_dashboard/features/orders/presentation/views/widgets/orders_view_body_bloc_builder.dart';
+import 'package:fruits_hub_dashboard/features/orders/presentation/views/widgets/update_order_bloc_consumer.dart';
 
 class OrdersView extends StatelessWidget {
   const OrdersView({super.key});
@@ -12,11 +14,18 @@ class OrdersView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => FetchOrdersCubit(getIt.get<OrdersRepo>()),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FetchOrdersCubit(getIt.get<OrdersRepo>()),
+        ),
+        BlocProvider(
+          create: (context) => UpdateOrderCubit(getIt.get<OrdersRepo>()),
+        ),
+      ],
       child: Scaffold(
         appBar: AppBar(title: Text('Orders'), centerTitle: true),
-        body: OrdersViewBodyBlocBuilder(),
+        body: UpdateOrderBlocConsumer(child: OrdersViewBodyBlocBuilder()),
       ),
     );
   }
